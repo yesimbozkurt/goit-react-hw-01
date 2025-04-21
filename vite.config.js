6d83f454c0a6c79269c7eb7e9562cd00cfa24d21
yesimbozkurt <yesimsbozkurt@gmail.com>
2025-04-21 23:01:07 +0300

    Remove unused environment variables from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import process from "process";
-const appName = process.env.npm_package_name;
-const appMode = process.env.NODE_ENV; // Development or Production
+
+
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: appMode === "development" ? "/" : `/${appName}/`
+
 })
